ac1e37b7fcdab310a5558455f1a2ad48f46f6c6a
rjpevans <ryanevans19971@gmail.com>
2023-02-19 21:54:19 +0000

    Added example snippet to entry point of program

--- CheckoutKata.Core/Program.cs
+++ CheckoutKata.Core/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace CheckoutKata.Core
+﻿using CheckoutKata.Core.Models;
+
+namespace CheckoutKata.Core
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ShoppingBasket basket = new ShoppingBasket();
+
+            Product A = new Product("A", 10);
+            Product B = new Product("B", 15);
+            Product C = new Product("C", 40);
+            Product D = new Product("D", 55);
+
+            Promotion PromoB = new Promotion("B", 3, 40);
+            Promotion PromoD = new Promotion("D", 2, 82.5m);
+
+            basket.AddPromotion(PromoB);
+            basket.AddPromotion(PromoD);
+
+            basket.AddProduct(B);
+            basket.AddProduct(B);
+            basket.AddProduct(B);
+
+            Console.WriteLine(basket.CalculateTotalPrice());
         }
     }
 }
