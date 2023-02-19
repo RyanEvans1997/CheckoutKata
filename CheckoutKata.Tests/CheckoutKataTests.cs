4fa3a1ea2a1a3da27faca21a0942be23a3b0f756
rjpevans <ryanevans19971@gmail.com>
2023-02-19 20:25:12 +0000

    Added xUnit to project, all tests are red/failing. Starting to add minimum code required to get them passing.

---
+++ CheckoutKata.Tests/CheckoutKataTests.cs
@@ -0,0 +1,101 @@
+namespace CheckoutKata.Tests
+{
+    public class CheckoutKataTests
+    {
+        [Fact]
+
+        public void Empty_Basket_Returns_Total_Zero()
+        {
+
+            Assert.Equal(0, 1);
+        }
+
+
+        [Fact]
+
+        public void Cannot_Add_Negative_Price_Item_To_Basket()
+        {
+            Assert.Equal(0, 1);
+        }
+
+
+        [Fact]
+
+        public void Individual_Products_Return_Correct_Price()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+
+        [Fact]
+
+        public void No_Promotion_Multiple_Products_Return_Correct_Price()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+
+        public void Promotion_B_Successfully_Applies()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+
+        public void Promotion_D_Successfully_Applies()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+
+        public void Multiple_Promotions_Successfully_Applies()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+
+        public void Promotions_Quantity_Only_Applies_Once()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+
+        public void Promotions_Quantity_Applies_Twice()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+
+        public void Normal_Customer_Base_Case_One()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+        public void Normal_Customer_Base_Case_Two()
+        {
+            Assert.Equal(0, 1);
+
+        }
+
+        [Fact]
+        public void Normal_Customer_Base_Case_Three()
+        {
+            Assert.Equal(0, 1);
+
+        }
+    }
+}
